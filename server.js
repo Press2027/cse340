dd39489fe6c23dc66db8ec97721bf0a8569a813d
press2025 <preston.machanga@aol.com>
2026-01-15 16:25:34 +0300

    added project

--- server.js
+++ server.js
@@ -1,30 +1,42 @@
-/* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
 /* ***********************
  * Require Statements
  *************************/
 const express = require("express")
-const env = require("dotenv").config()
+const expressLayouts = require("express-ejs-layouts")
+const path = require("path")
+require("dotenv").config()
+
 const app = express()
-const static = require("./routes/static")
+const staticRoutes = require("./routes/static")
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
+
+/* ***********************
+ * Static Files Middleware
+ *************************/
+app.use(express.static(path.join(__dirname, "public")))
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(staticRoutes)
+
+// Index route
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
- * Local Server Information
- * Values from .env (environment) file
+ * Server Information
  *************************/
 const port = process.env.PORT
 const host = process.env.HOST
 
-/* ***********************
- * Log statement to confirm server operation
- *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
